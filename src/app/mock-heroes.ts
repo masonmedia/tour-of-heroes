e3f9f716732dc4fb217a9179d6e602534e0f57ad
Andrew Mason <andrewmasonmedia@gmail.com>
2019-06-06 16:46:16 -0400

    Update to hero class, hero.ts

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,14 +1,14 @@
 import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
-    { id: 11, name: 'Dr. Nice' },
-    { id: 12, name: 'Narco' },
-    { id: 13, name: 'Bombasto' },
-    { id: 14, name: 'Celeritas' },
-    { id: 15, name: 'Magneta' },
-    { id: 16, name: 'Rubberman' },
-    { id: 17, name: 'Dynama' },
-    { id: 18, name: 'Dr. IQ' },
-    { id: 19, name: 'Magma' },
-    { id: 20, name: 'Tornado' }
+    { id: 11, name: 'Dr. Nice', shoesize: 10 },
+    { id: 12, name: 'Narco', shoesize: 13 },
+    { id: 13, name: 'Bombasto', shoesize: 11 },
+    { id: 14, name: 'Celeritas', shoesize: 9 },
+    { id: 15, name: 'Magneta', shoesize: 12 },
+    { id: 16, name: 'Rubberman', shoesize: 11 },
+    { id: 17, name: 'Dynama', shoesize: 8 },
+    { id: 18, name: 'Dr. IQ', shoesize: 7 },
+    { id: 19, name: 'Magma', shoesize: 11 },
+    { id: 20, name: 'Tornado', shoesize: 10 }
 ] 
